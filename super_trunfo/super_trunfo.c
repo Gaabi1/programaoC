12c0330cabed20c372045769b805a3d51caa8c62
Gabi <143514875+Gaabi1@users.noreply.github.com>
2025-03-07 15:19:50 +0000

    Novo commit1

--- super_trunfo/super_trunfo.c
+++ super_trunfo/super_trunfo.c
@@ -11,7 +11,9 @@ int main()
     float PIB;
     float area;
     int turistico;
-    
+    float densidade_populacional;
+    float PIB_per_capita;
+
     printf("Carta 1\n");
     
     printf("Estado letra A a H: ");
@@ -37,6 +39,9 @@ int main()
     printf("Número de Pontos Turísticos: ");
     scanf("%i",&turistico);
 
+    densidade_populacional= (float) populacao /area;
+    PIB_per_capita= PIB / (float)populacao;
+
     char estado2;
     char numero2[4];
     char nome2[40];
@@ -44,6 +49,8 @@ int main()
     float PIB2;
     float area2;
     int turistico2;
+    float densidade_populacional2;
+    float PIB_per_capita2;
     
     printf("Carta 2\n");
     
@@ -70,21 +77,31 @@ int main()
     printf("Número de Pontos Turísticos: ");
     scanf("%i",&turistico2);
 
-    printf("------Carta 1------- \nEstado: %c\n", estado); 
+    densidade_populacional2= (float) populacao2/area2;
+    PIB_per_capita2= PIB2 / (float)populacao2;
+
+    printf("------Carta 1------- \n");
+    printf("Estado: %c\n", estado); 
     printf("Código: %s\n", numero);
     printf("Nome da cidade: %s\n", nome);
     printf("População: %i\n", populacao);
     printf("Área: %f\n", area); 
     printf("PIB: %f\n", PIB); 
     printf("Número de Pontos Turísticos: %i\n",turistico);
+    printf("Densidade Populacional: %.2f\n",densidade_populacional);
+    printf("O PIB per capita é: %.2f\n", PIB_per_capita);
 
-    printf("------Carta 2------ \nEstado: %c\n", estado2); 
+    printf("------Carta 2------ \n");
+    printf("Estado: %c\n", estado2); 
     printf("Código: %s\n", numero2);
     printf("Nome da cidade: %s\n", nome2);
     printf("População: %i\n", populacao2);
     printf("Área: %f\n", area2); 
     printf("PIB: %f\n", PIB2); 
     printf("Número de Pontos Turísticos: %i\n",turistico2);
+    printf("Densidade populacional: %.2f\n", densidade_populacional2);
+    printf("O PIB per capita é: %.2f\n", PIB_per_capita2);
 
+    return 0;
 
 }
